7351c9b7974a4f4a3abc81b3dfaba9d1ba20b909
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-02-14 14:42:51 +0000

    Production config in use.

---
+++ pulse-deploy/prod/setup-prod-db.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+source /etc/sysconfig/w3act
+
+scp root@act.wa.bl.uk:/backup/postgres_db/w3act.pgdump-20170214095311 w3act.pgdump
+cp w3act.pgdump /zfspool/prod/postgresql/
+
+./docker-prod.sh up -d postgres
+
+sleep 10
+
+echo "Importing data..."
+./docker-prod.sh exec postgres pg_restore -d w3act -U w3act -O /var/lib/postgresql/data/w3act.pgdump
+echo "First upgrade to 2.0.x needs a TRUNCATE watched_target CASCADE; !!!"
+
+sleep 10
+
+echo "Shutting down..."
+./docker-prod.sh down
+
