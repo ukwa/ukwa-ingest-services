9e56f591905b43af6b810d63cb75b493fbac8802
Andrew Jackson <Andrew.Jackson@bl.uk>
2017-03-29 11:01:20 +0100

    update w3act.

--- pulse-deploy/prod/resume-all-crawlers.sh
+++ pulse-deploy/prod/resume-all-crawlers.sh
@@ -2,7 +2,7 @@
 
 ./docker-prod.sh exec shepherd pulse resume daily
 ./docker-prod.sh exec shepherd pulse resume weekly
-./docker-prod.sh exec shepherd pulse start monthly
+./docker-prod.sh exec shepherd pulse resume monthly
 ./docker-prod.sh exec shepherd pulse resume quarterly
 ./docker-prod.sh exec shepherd pulse resume sixmonthly
 ./docker-prod.sh exec shepherd pulse resume annual
