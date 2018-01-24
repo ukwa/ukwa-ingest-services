a82e4f05728585150170ba9394015779166cf200
root <root@crawler01.n45.bl.uk>
2018-01-24 12:29:59 +0000

    Current production deployment configuration.

--- deploy/frontend/prod/docker-prod.sh
+++ deploy/frontend/prod/docker-prod.sh
@@ -4,4 +4,4 @@
 source /etc/sysconfig/w3act
 
 # Launch with correct combined configuration:
-docker-compose -f ../docker-compose.yml -f docker-compose.prod.yml -p pulse_prod "$@"
+docker-compose -f ../docker-compose.yml -f docker-compose.prod.yml -p pulsefeprod "$@"
