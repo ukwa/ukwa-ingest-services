56424aafe5612d2627074c6465950f266f751b68
root <root@ingest.n1.wa.bl.uk>
2019-02-12 10:22:48 +0000

    New setup with pywb.

--- deploy/frontend/beta/deploy-w3act-beta.sh
+++ deploy/frontend/beta/deploy-w3act-beta.sh
@@ -2,6 +2,7 @@
 
 # Pull in configuration environment variables:
 source /etc/sysconfig/w3act-beta
+export PYWB_NGINX_CONF=$PWD/pywb-nginx.conf
 
 # Launch with correct combined configuration:
-docker stack deploy -c docker-compose.yml ife_beta
+docker stack deploy -c ../docker-compose.yml ife_beta
