7e486335924a68d42aee0f50535f82669e127be1
root <root@crawler05.n45.bl.uk>
2020-02-03 14:59:28 +0000

    Current production setup.

--- deploy/fc/prod/prod-env.sh
+++ deploy/fc/prod/prod-env.sh
@@ -1,9 +1,9 @@
 export CRAWL_HOST_LAN_IP=192.168.45.15
 export CRAWL_HOST_WAN_IP=194.66.232.83
 export STORAGE_PATH=/mnt/gluster/fc
-# BigCDX2
-#export CDXSERVER_ENDPOINT=http://192.168.45.22:9090/fc
-# BigCDX
-export CDXSERVER_ENDPOINT=http://192.168.45.21:9090/fc
-export HERITRIX_VERSION=2.6.1
+# CrawlDB FC (OutbackCDX)
+# Oddly slow...
+#export CDXSERVER_ENDPOINT=http://crawldb-fc.api.wa.bl.uk/fc
+export CDXSERVER_ENDPOINT=http://192.168.45.8:8081/fc
+export HERITRIX_VERSION=2.7.0-BETA-3
 
